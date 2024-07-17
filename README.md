80814a1c7d7e1a148c4734786c1f6b0cd428110f
xxkfqz <xxkfqz@gmail.com>
2024-07-17 15:38:09 +0500

    Readable version

---
+++ README.md
@@ -0,0 +1,10 @@
+# cpugov
+
+Simple perl script to manage CPU governors on *NIX systems
+
+## Usage:
+
+```perl
+cpugov.pl               # Print available governors
+cpugov.pl [some_name]   # Set governor
+```
--- cpugov.pl
+++ cpugov.pl
@@ -1,34 +1,60 @@
 #!/usr/bin/perl
-# Usage:
-#   cpugov.pl             -> print available governors
-#   cpugov.pl [some_name] -> set governor
+# </> with <3, xxkfqz 2d24
 
 use warnings;
 use strict;
 
 my $baseDir = '/sys/devices/system/cpu';
 
+sub getCurrentGovernor {
+    open(FH, '<', "$baseDir/cpu0/cpufreq/scaling_governor") || die "Could not read governor: $!";
+    (my $curGovernor = <FH>) =~ s/\R//g;
+    close FH;
+
+    $curGovernor;
+}
+
+sub getAvailabeGovernors {
+    my $govListFile = "$baseDir/cpu0/cpufreq/scaling_available_governors";
+    open(FH, '<', $govListFile)
+        || die "Could not open '$govListFile': $!";
+    my @availableGoverors = split(' ', <FH>);
+    close FH;
+
+    @availableGoverors;
+}
+
+my $currentGovernor = getCurrentGovernor;
+my @availableGoverors = getAvailabeGovernors;
+
 if ($#ARGV < 0) {
     # Just print available governors
-    open(FH, '<', "$baseDir/cpu0/cpufreq/scaling_governor") || die "Could not read governor: $!";
-    (my $curGov = <FH>) =~ s/\R//g;
-    close FH;
+    print "Available governors:\n";
 
-    my $govListFile = "$baseDir/cpu0/cpufreq/scaling_available_governors";
-    open(FH, '<', $govListFile) || die "Could not open '$govListFile': $!";
-    my $availGovs = join("\n\t", split ' ', <FH>) =~ s/(?<=$curGov)/\t\(current\)/gr;
-    print "Available governors:\n\t$availGovs\n";
-    close FH;
+    for (@availableGoverors) {
+        my $ch = '  ';
+        if ($_ eq $currentGovernor) {
+            $ch = '->';
+        }
+        print " $ch $_\n";
+    }
 } else {
     # Set governor
-    opendir(my $dirHandler, $baseDir) || die "Could not open directory '$baseDir': $!\n";
-    my @cpusList = sort grep /^cpu\d+$/, readdir $dirHandler;
+    my $newGovernor = shift;
+    grep(/^$newGovernor$/, @availableGoverors)
+        || die "Governor '$newGovernor' not available\n";
+
+    opendir(my $dirHandler, $baseDir)
+        || die "Could not open directory '$baseDir': $!\n";
+    my @cpusList = sort(grep(/^cpu\d+$/, readdir($dirHandler)));
     closedir $dirHandler;
 
-    my $newGov = shift;
+    print "Switching from '$currentGovernor' to '$newGovernor'\n";
+
     for (@cpusList) {
-        open(FH, '>', "$baseDir/$_/cpufreq/scaling_governor") || die "Could not set governor '$newGov' on $_: $!\n";
-        print FH $newGov;
+        open(FH, '>', "$baseDir/$_/cpufreq/scaling_governor")
+            || die "Could not set governor '$newGovernor' on $_: $!\n";
+        print FH $newGovernor;
         close FH;
     }
 }
